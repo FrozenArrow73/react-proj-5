e7c1d67893953cfa2636b25a20c49ffb79d97ca8
Derek Wheeler <frozenarrow73@gmail.com>
2023-05-11 13:40:38 -0600

    completed symbols.js

--- src/Components/Symbols.js
+++ src/Components/Symbols.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectDisplay } from '../redux/slices/displayCountrySlice'
+
 
 function Symbols() {
+  let currentDisplay = useSelector(selectDisplay)
   return (
-    <div>Symbols</div>
+    <div className='symbols'>
+      <div className='stack'>
+        <h2>
+          Flag of {currentDisplay.name.common}:
+        </h2>
+        {currentDisplay.flags ? (
+          <img src={currentDisplay.flags.png}/>
+        ) : (
+          "(no data found)"
+        )}
+      </div>
+      <div className='stack'>
+        <h2>
+          Coat of arms of {currentDisplay.name.common}:
+        </h2>
+        {currentDisplay.coatOfArms ? (
+          <img src={currentDisplay.coatOfArms.png}/>
+        ) : (
+          "(no data found)"
+        )}
+      </div>
+    </div>
   )
 }
 
