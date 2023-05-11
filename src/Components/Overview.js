1cf4a96061b6a5845fd1d933912f1086aaadeb89
Derek Wheeler <frozenarrow73@gmail.com>
2023-05-11 12:42:53 -0600

    completed overview.js page

--- src/Components/Overview.js
+++ src/Components/Overview.js
@@ -4,8 +4,36 @@ import {selectDisplay} from "../redux/slices/displayCountrySlice"
 
 function Overview() {
   let currentDisplay = useSelector(selectDisplay)
+  const officialNameDisplay = currentDisplay.name.official
+  const commonNameDisplay = currentDisplay.name.common
+  const capitalDisplay = currentDisplay.capital
+  const continentDisplay = currentDisplay.continents
+  const populationDisplay = currentDisplay.population
+  const memberOfUnDisplay = currentDisplay.unMember ? "True" : "False"
+  
+
   return (
-    <div className='stack'>Overview</div>
+    <div className='stack'>
+      <h1>
+        {officialNameDisplay}
+      </h1>
+      <h2>
+        {commonNameDisplay}
+      </h2>
+
+      <h3>
+        Capital: {capitalDisplay}
+      </h3>
+      <h3>
+        Continent: {continentDisplay}
+      </h3>
+      <h3>
+        Population: {populationDisplay}
+      </h3>
+      <h3>
+        Is a member of the UN: {memberOfUnDisplay}
+      </h3>
+    </div>
   )
 }
 
