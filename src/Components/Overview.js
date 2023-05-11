684083fe9f58873ce7516b787173438af5f3789b
Derek Wheeler <frozenarrow73@gmail.com>
2023-05-11 12:03:30 -0600

    overview prepared to display data

--- src/Components/Overview.js
+++ src/Components/Overview.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import {selectDisplay} from "../redux/slices/displayCountrySlice"
 
 function Overview() {
+  let currentDisplay = useSelector(selectDisplay)
   return (
-    <div>Overview</div>
+    <div className='stack'>Overview</div>
   )
 }
 
